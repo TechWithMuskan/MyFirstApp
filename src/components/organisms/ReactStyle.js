5711a601ed6acdb590ecd996db1134e3663c5f42
TechWithMuskan <muskanjain16apr@gmail.com>
2021-07-18 09:59:58 +0530

    feat(style): various type
    inline style,
    style with stylesheet,
    mutiple style,
    external style

---
+++ src/components/organisms/ReactStyle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const ReactStyle = () => {
+    return (
+        <View>
+            <Text style={styles.textStyle}>Hello World</Text>
+        </View>
+    )
+}
+
+export default ReactStyle;
